97b72161c7339a5f3ae9e100984fda6e73e6be4d
Maxim Akimov <akimovmax@mail.ru>
2017-04-13 20:18:22 +0300

    Выполнил задание #10 Учебный проект: одеть Надежду

--- js/setup.js
+++ js/setup.js
@@ -37,13 +37,23 @@ var WIZARD_EYES_COLOR = [
   'yellow',
   'green'
 ];
+var WIZARD_FIREBALL_COLOR = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
 var WIZARD_QUANTITY = 4;
 
 // Объявляем переменную, внутри которой находится DIV всплывающего окна с настройками мага
 var userDialog = document.querySelector('.setup');
 
-// Делаем DIV с настройками мага видимым, удаляя класс 'hidden'
-userDialog.classList.remove('hidden');
+// Объявляем переменую, внутри которой находится DIV '.setup-open'
+var userDialogOpen = document.querySelector('.setup-open');
+
+// Объявляем переменую, внутри которой находится SPAN '.setup-close'
+var userDialogClose = userDialog.querySelector('.setup-close');
 
 // Объявляем переменную, внутри которой находится пустой DIV списка похожих персонажей
 var similarListElement = userDialog.querySelector('.setup-similar-list');
@@ -93,3 +103,79 @@ var drawAllWizards = function () {
 };
 
 drawAllWizards();
+
+// Объявляем функцию закрытия окна попап по нажатию на ESC
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === 27) {
+    closePopup();
+  }
+};
+
+// Объявляем функцию открытия окна попап
+var openPopup = function () {
+  userDialog.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+// Объявляем функцию закрытия окна попап
+var closePopup = function () {
+  userDialog.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+// Опишем реакцию блока '.setup-open' на клик мыши
+userDialogOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+// Опишем реакцию блока '.setup-open' на нажатие ENTER на сфокусированном блоке '.setup-open-icon'
+userDialogOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    openPopup();
+  }
+});
+
+// Опишем реакцию блока '.setup-close' на клик мыши
+userDialogClose.addEventListener('click', function () {
+  closePopup();
+});
+
+// Опишем реакцию блока '.setup-close' на нажатие ENTER на сфокусированном блоке '.setup-close'
+userDialogClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 13) {
+    closePopup();
+  }
+});
+
+// Остановим распространение события при нажатии на ESC, в случае фокуса на элементе '.setup-user-name'
+document.querySelector('.setup-user-name').addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    evt.stopPropagation();
+  }
+});
+
+// Добавим в окно попап функционал изменения по клику мыши:
+// 1) цвет плаща мага из набора 'WIZARD_COAT_COLOR';
+// 2) цвет глаз мага из набора 'WIZARD_EYES_COLOR';
+// 2) цвет фаербола из набора 'WIZARD_FIREBALL_COLOR'.
+
+// Задаем необходимые переменные
+var wizard = document.querySelector('.setup-wizard');
+var wizardCoat = wizard.querySelector('.wizard-coat');
+var wizardEyes = wizard.querySelector('.wizard-eyes');
+var wizardFireball = document.querySelector('.setup-fireball-wrap');
+
+// 1)
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = getRandomValue(WIZARD_COAT_COLOR);
+});
+
+// 2)
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = getRandomValue(WIZARD_EYES_COLOR);
+});
+
+// 3)
+wizardFireball.addEventListener('click', function () {
+  wizardFireball.style.background = getRandomValue(WIZARD_FIREBALL_COLOR);
+});
